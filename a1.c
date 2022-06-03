2a8ef009f22ee32d1f757909f1f1d2cb7fda869f
1703manish <94585964+1703manish@users.noreply.github.com>
2022-06-03 10:46:32 +0530

    Update a1.c

--- a1.c
+++ a1.c
@@ -5,6 +5,10 @@ void main()
 {
 int a=10;//local variable
 
+auto int b=20;//local variable again but used auto with int b
+  
 printf("local variable a value is %d\n",a);
+
+printf("local variable b value is %d\n",b);
 
 }
